a59f5df408c197973ad0c4ceecce26733086752c
Taylor Somers <tsomers@yandex.com>
2020-06-15 13:06:24 -0400

    Separate Address Book business logic from Contacts business logic and create prototype to add contacts to an address book

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,19 @@
 // Business Logic:
 
+  // Business Logic for Address Book:
+
+function AddressBook() {
+  this.contacts = []
+}
+
+AddressBook.prototype.addContact = function(contact) {
+  this.contacts.push(contact);
+}
+
+  // End Business Logic for Address Book
+
+  // Business Logic for Contacts:
+
 function Contact(firstName, lastName, phoneNumber) {
   this.firstName = firstName;
   this.lastName = lastName;
@@ -9,6 +23,8 @@ function Contact(firstName, lastName, phoneNumber) {
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
+
+  // End Business Logic for Contacts
 
 // End Business Logic
 
