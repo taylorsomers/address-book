48a3d837e6daad72655aa579984bb3819328e072
Taylor Somers <tsomers@yandex.com>
2020-06-15 12:44:10 -0400

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+// Business Logic:
+
+function Contact(firstName, lastName, phoneNumber) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.phoneNumber = phoneNumber;
+}
+
+// End Business Logic
+
+// UI Logic
+
+$(document).ready(function() {
+
+});
+
+// End UI Logic
