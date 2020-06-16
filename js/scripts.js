82d5643fb8892557b25014c9b1c19cdb01bcd36e
Taylor Somers <tsomers@yandex.com>
2020-06-16 10:11:10 -0400

    Add UI logic to initiate creation of new contact when the new-contact form is submitted

--- js/scripts.js
+++ js/scripts.js
@@ -60,8 +60,18 @@ Contact.prototype.fullName = function() {
 
 // UI Logic
 
-$(document).ready(function() {
+let addressBook = new AddressBook();
 
+$(document).ready(function() {
+  $("form#new-contact").submit(function(event) {
+    event.preventDefault();
+    const inputtedFirstName = $("input#new-first-name").val();
+    const inputtedLastName = $("input#new-last-name").val();
+    const inputtedPhoneNumber = $("input#new-phone-number").val();
+    let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
+    addressBook.addContact(newContact);
+    console.log(addressBook.contacts);
+  });
 });
 
 // End UI Logic
